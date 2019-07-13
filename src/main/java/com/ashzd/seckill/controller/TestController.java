cb551ccd331161bd219e8faea5abca4a1b1759a5
NormalAsh <ash_zd@qq.com>
2019-07-13 16:35:36 +0800

    【修改】更新初始化项目细节

---
+++ src/main/java/com/ashzd/seckill/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ashzd.seckill.controller;
+
+import com.ashzd.seckill.controller.common.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @file: TestController
+ * @author: Ash
+ * @date: 2019/7/13 16:11
+ * @description:
+ * @since:
+ **/
+@RestController
+@RequestMapping("/test")
+public class TestController extends BaseController {
+
+}
---
+++ src/main/java/com/ashzd/seckill/controller/common/BaseController.java
@@ -0,0 +1,14 @@
+package com.ashzd.seckill.controller.common;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @file: BaseController
+ * @author: Ash
+ * @date: 2019/7/13 16:11
+ * @description:
+ * @since:
+ **/
+@RestController
+public class BaseController {
+}
