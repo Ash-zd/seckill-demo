0aeff1ecd3e8e42c5b908d07f6fc740e54252ba1
NormalAsh <ash_zd@qq.com>
2019-07-18 23:53:44 +0800

    【修改】强制设定fastjson为默认序列化、反序列化converter

--- src/main/java/com/ashzd/seckill/config/fastjson/FastJsonConfig.java
+++ src/main/java/com/ashzd/seckill/config/fastjson/FastJsonConfig.java
@@ -1,10 +1,15 @@
 package com.ashzd.seckill.config.fastjson;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * @file: FastJsonConfig
@@ -14,7 +19,7 @@ import org.springframework.data.redis.core.RedisTemplate;
  * @since:
  **/
 @Configuration
-public class FastJsonConfig {
+public class FastJsonConfig implements WebMvcConfigurer {
 
     /**
      * 在 Spring Data Redis 中集成 Fastjson
@@ -35,6 +40,21 @@ public class FastJsonConfig {
         // 单独设置valueSerializer
         // redisTemplate.setValueSerializer(fastJsonRedisSerializer);
         return redisTemplate;
+    }
+
+    /**
+     * 强制设定fastjson为默认序列化、反序列化converter
+     *
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        // 自定义配置...
+//        FastJsonConfig config = new FastJsonConfig();
+//        config.set ...
+//        converter.setFastJsonConfig(config);
+        converters.add(0, converter);
     }
 
 }
